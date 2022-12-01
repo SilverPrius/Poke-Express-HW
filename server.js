e883742e4570175a22ae660ae355ed13d99dfcee
Long Hoang <long88hoang@gmail.com>
2022-12-01 11:53:09 -0600

    Connected my database, can see json in the browser

--- server.js
+++ server.js
@@ -4,6 +4,21 @@ const express = require('express')
 //Create express app
 const app = express()
 
+const pokemon = []
+
+//Index route = SHOW ALL
+app.get('/pokemon/', (req, res) => {
+    res.send(pokemon)
+})
+
+//New = GET A FORM TO CREATE A NEW RECORD
+//Delete - DELETE
+//Update - MODIFYING A RECORD
+//Create - SEND THE FILLED FORM TO DATABASE AND CREATE A NEW RECORD
+//Edit - GO TO DATABASE AND GET THE RECORD TO UPDATE
+
+//Show route = SHOW ME A PARTICULAR RECORD
+
 //Set port
 const port = 3000
 
