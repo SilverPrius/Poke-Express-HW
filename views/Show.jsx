c5826cdc40116b7ed5a21597775a43787535b2c7
Long Hoang <long88hoang@gmail.com>
2022-12-02 10:49:56 -0600

    Created show views of each pokemon

--- views/Show.jsx
+++ views/Show.jsx
@@ -14,6 +14,7 @@ class Show extends React.Component {
                 <h1 style={myStyle} >Gotta Catch 'Em All</h1>
                 <h2>{pokemon.name[0].toUpperCase() + pokemon.name.slice(1)}</h2>
                 <img src={pokemon.img + '.jpg'}></img>
+                <a href="/pokemon">Back</a>
     
             </div>
         );
