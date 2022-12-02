6c0c774941e3a2f297f5fd909c30ae1fa2e9c71a
Long Hoang <long88hoang@gmail.com>
2022-12-02 11:00:05 -0600

    Clean up

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const app = express()
 
 const pokemon = require('./models/pokemon.js')
 
+//Set port
+const port = 3000
 
 //Setup jsx view engine
 app.set('view engine', 'jsx')
@@ -14,30 +16,26 @@ app.engine('jsx', require('express-react-views').createEngine())
 
 //Index route = SHOW ALL
 app.get('/pokemon/', (req, res) => {
-    res.render('Index', {pokemon: pokemon})
+    res.render('Index', { pokemon: pokemon })
 })
 
-app.get('/pokemon/:id', (req, res) => {
-	res.render('Show', {pokemon: pokemon[req.params.id]})
-})
-
-//New = GET A FORM TO CREATE A NEW RECORD
+//New - GET A FORM TO CREATE A NEW RECORD
 //Delete - DELETE
 //Update - MODIFYING A RECORD
 //Create - SEND THE FILLED FORM TO DATABASE AND CREATE A NEW RECORD
 //Edit - GO TO DATABASE AND GET THE RECORD TO UPDATE
 
 //Show route = SHOW ME A PARTICULAR RECORD
-
-//Set port
-const port = 3000
-
 app.get('/', (req, res) => {
     res.send('Welcome to the Pokemon App!')
 })
 
+app.get('/pokemon/:id', (req, res) => {
+    res.render('Show', { pokemon: pokemon[req.params.id] })
+})
 
 
 app.listen(port, () => {
-    console.log(`Listening to port ${port}` )
+    console.log(`Listening to port ${port}`)
 })
+
