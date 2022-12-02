e9ef81298619c3a8a7a96d4c7b0ad015b6fed11d
Long Hoang <long88hoang@gmail.com>
2022-12-02 10:41:27 -0600

    Created show views of each pokemon

--- views/Show.jsx
+++ views/Show.jsx
@@ -1,15 +1,26 @@
-const React = require('react');
-class Show extends React.Component {
-  render() {
-    const pokemon = this.props.pokemon;
-    return (
-      <div>
-        Hello World!
-      </div>
-    );
-  }
+const React = require('react')
+
+const myStyle = {
+    color: 'yellow',
+    backgroundColor: 'blue',
 };
 
+
+class Show extends React.Component {
+    render() {
+        const { pokemon } = this.props;
+        return (
+            <div>
+                <h1 style={myStyle} >Gotta Catch 'Em All</h1>
+                <h2>{pokemon.name[0].toUpperCase() + pokemon.name.slice(1)}</h2>
+                <img src={pokemon.img + '.jpg'}></img>
+    
+            </div>
+        );
+    }
+}
+
 module.exports = Show;
+
 
 
